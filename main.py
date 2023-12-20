5a17cb721423b28db3de94076fcf9214cad9d7c0
Your_Void <my.vo1d.edge@gmail.com>
2023-12-20 09:47:53 +0300

    Изменение получения нормализированных low  и high

--- main.py
+++ main.py
@@ -1,57 +1,65 @@
 import random
+from bitarray import bitarray
 
-def compress(data):
+def compress(data, compressed_file):
     # Подсчет частоты символов
     freq = {}
     for symbol in data:
         freq[symbol] = freq.get(symbol, 0) + 1
     sorted_freq = dict(sorted(freq.items(), key=lambda item: item[1], reverse=True))
-
+    
     # Создание вложенных интервалов для каждого символа
     intervals = {}
     low = 0
-    high = 2**32
+    high = 65535
+    low_last = 0
+    high_last = 65535
+    bits_F = 0
+    
+    qtr_1 = (high_last + 1) / 4
+    half = qtr_1 * 2
+    qtr_3 = qtr_1 * 3
+
     for symbol, count in sorted_freq.items():
         high = low + count
         intervals[symbol] = (low, high)
         low = high
-
-    # Инициализация границ интервала для кодирования
-    low_last = 0
-    high_last = 65_535
-    qtr_1 = (high_last + 1) / 4
-    qtr_3 = qtr_1 * 3
-    half = qtr_1 * 2
-    bits_break = 0
     
-    # Кодирование данных
-    for symbol in data:
-        symbol_range = intervals[symbol]
-        range_size = high_last - low_last
-        low = low_last + symbol_range[0] * range_size
-        high = low_last + symbol_range[1] * range_size
+    last_key = list(intervals.keys())[-1]
+    # Получение значения по последнему ключу
+    delit = intervals[last_key][1]
 
+    for symbol in data:
+
+        print(intervals[symbol][0] * (high_last - low_last + 1) / delit) 
+        low = int(low_last + intervals[symbol][0] * (high_last - low_last + 1) / delit)
+        high = int(low_last + intervals[symbol][1] * (high_last - low_last + 1) / delit - 1)
+        print("Lo "+ str(low))
+        print("Hi "+ str(high))
+        quit()
+        while True:
+            if low < half:
+                print("0")
+            elif low >= half:
+                print("1")
+                low -= half
+                high -= half
+            elif low >= qtr_3 and high < qtr_1:
+                bits_F += 1
+                low -= qtr_1
+                high -= qtr_1
+            else:
+                break
+            low += low
+            high += high + 1
         low_last = int(low)
         high_last = int(high)
-    
-    while True:  # Обрабатываем варианты переполнения
-        if high_last < half:
-            pass
-            #Битовая операция
-        elif low_last >= half:
-            #Битовая операция
-            pass
-            low_last -= half
-            high_last -= half
-        elif low_last >= qtr_3 and high_last < qtr_1:
-            bits_to_follow += 1
-            low_last -= qtr_1
-            high_last -= qtr_1
-        else:
-            break
-        low_last += low_last
-        high_last += high_last + 1
-        
-    return low_last, high_last, intervals
+        print("Lo "+ str(low_last))
+        print("Hi "+ str(high_last))
 
+
+comp = "compressed.txt"
+data = "КОВ.КОРОВА"
+compress(data, comp)
+print("Compressed Data:", compress(data))
 
